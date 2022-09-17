8e0394ffa13e289a1631c64ee15e75aeb2ac9182
nejatkitis <46318136+nejatkitis@users.noreply.github.com>
2022-09-18 00:19:52 +0300

    Add files via upload

---
+++ Asal.java
@@ -0,0 +1,11 @@
+public class Asal {
+    public static void main(String[] args) {
+
+        System.out.print("2,3,5,7");
+        for (int i =8;i<100;i++){
+            if ((i%2!=0)&&(i%3!=0)&&(i%5!=0)&&(i%7!=0)){
+                System.out.print(","+i);
+            }
+        }
+    }
+}
